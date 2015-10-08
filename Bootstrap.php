6c7f707b1a00f95c5f05bf571d890d0bc10c7514
Michal Fiedorowicz <michal@qi-interactive.com>
2015-10-08 15:36:50 +0100

    - Changed ActiveQuery::EVENT_BEFORE_PREPARE_STATEMENT to run only for models with HistoryBehavior

--- Bootstrap.php
+++ Bootstrap.php
@@ -32,23 +32,26 @@ class Bootstrap extends \mata\base\Bootstrap {
 			$activeQuery = $event->sender;
 			$modelClass = $activeQuery->modelClass;
 			$sampleModelObject = new $modelClass;
-			$documentIdBase = $sampleModelObject->getDocumentId()->getId();
-			$tableAlias = $activeQuery->getQueryTableName($activeQuery)[0];
 
-			if (count($modelClass::primaryKey()) > 1) {
-				throw new HttpException(500, sprintf("Composite keys are not handled yet. Table alias is %s", $tableAlias));
+			if (BehaviorHelper::hasBehavior($sampleModelObject, \mata\arhistory\behaviors\HistoryBehavior::class)) {		
+				$documentIdBase = $sampleModelObject->getDocumentId()->getId();
+				$tableAlias = $activeQuery->getQueryTableName($activeQuery)[0];
+
+				if (count($modelClass::primaryKey()) > 1) {
+					throw new HttpException(500, sprintf("Composite keys are not handled yet. Table alias is %s", $tableAlias));
+				}
+
+				$tablePrimaryKey = $modelClass::primaryKey()[0];
+
+				// do we need this?
+				// if ($activeQuery->join)
+				// 	foreach ($activeQuery->join as $join) {
+				// 		$tableToJoin = $join[1];
+				// 	 	$this->addArhistoryJoin($activeQuery, $tableToJoin  . ".DocumentId", $documentIdBase);
+				// 	}
+
+				$this->addArhistoryJoin($activeQuery, "CONCAT('" . $documentIdBase . "', " . $tableAlias . "." . $tablePrimaryKey . ")", $documentIdBase);
 			}
-
-			$tablePrimaryKey = $modelClass::primaryKey()[0];
-
-			// do we need this?
-			// if ($activeQuery->join)
-			// 	foreach ($activeQuery->join as $join) {
-			// 		$tableToJoin = $join[1];
-			// 	 	$this->addArhistoryJoin($activeQuery, $tableToJoin  . ".DocumentId", $documentIdBase);
-			// 	}
-
-			$this->addArhistoryJoin($activeQuery, "CONCAT('" . $documentIdBase . "', " . $tableAlias . "." . $tablePrimaryKey . ")", $documentIdBase);
 		});
 	}
 
@@ -64,8 +67,8 @@ class Bootstrap extends \mata\base\Bootstrap {
 		 * If is it not, it means that environments are not used for a given [[documentId]]
 		 * This check cannot be done with [[BehaviorHelper::hasBehavior]], as we not always have
 		 * the model class name, but always have the [[documentId]]
-		 */   
-		
+		 */
+
 		$hasVersions = Revision::find()->where(['like', 'DocumentId', $documentIdBase])->limit(1)->one();
 
 		if ($hasVersions == null)
@@ -75,27 +78,27 @@ class Bootstrap extends \mata\base\Bootstrap {
 
 		// TODO This encoding happens in Yii, use what they're offering. E.g. it is used in the call on line 91
 		$documentId = str_replace("\\", "\\\\\\",  $documentId);
-		
+
 		if ($activeQuery->select == null)
 			$activeQuery->addSelect(["*", $alias . ".Revision"]);
-		else 
+		else
 			$activeQuery->addSelect($alias . ".Revision");
 
-		$activeQuery->innerJoin("arhistory_revision " . $alias, "`" . $alias . "`.`DocumentId` = " . $documentId . " AND " . 
+		$activeQuery->innerJoin("arhistory_revision " . $alias, "`" . $alias . "`.`DocumentId` = " . $documentId . " AND " .
 				$alias . ".Revision = " . $alias . ".Revision AND " . $alias . ".Status = 1");
-		$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM arhistory_revision " . $alias . "rev WHERE " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId 
+		$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM arhistory_revision " . $alias . "rev WHERE " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId
 			 	 		 			 ORDER BY Revision DESC LIMIT 1)");
 
 		 // // TODO refactor and use Query!
 		 // if (Yii::$app->user->isGuest) {
 		 // 	$liveEnvironment = $module->getLiveEnvironment();
 
-		 // 	$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM matacms_itemenvironment " . $alias . "rev WHERE . " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId 
+		 // 	$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM matacms_itemenvironment " . $alias . "rev WHERE . " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId
 		 // 	 			 AND " . $alias . "rev.`Status` = '" . $liveEnvironment . "' ORDER BY " . $alias . ".Revision DESC LIMIT 1)");
 	 	// } else {
-	 	// 	$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM matacms_itemenvironment " . $alias . "rev WHERE " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId 
+	 	// 	$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM matacms_itemenvironment " . $alias . "rev WHERE " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId
 	 	// 		 			 ORDER BY " . $alias . ".Revision DESC LIMIT 1)");
-	 		
+
 	 	// }
 	}
 
@@ -111,10 +114,10 @@ class Bootstrap extends \mata\base\Bootstrap {
 	// 	if (BehaviorHelper::hasBehavior($model, \mata\arhistory\behaviors\HistoryBehavior::class))
 	// 		$model->setRevision($revision);
 	// }
-	
+
 	// private function processSave($model) {
 
-	// 	if (is_object($model) == false || 
+	// 	if (is_object($model) == false ||
 	// 		BehaviorHelper::hasBehavior($model, \mata\arhistory\behaviors\HistoryBehavior::class) == false)
 	// 		return;
 
